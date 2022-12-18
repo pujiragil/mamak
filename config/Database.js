72cc63e817ef02ca5e29e5ce33b1e8e414fade6a
pujiragil <ragilsg164@gmail.com>
2022-12-18 20:51:52 +0700

    create new user model

--- config/Database.js
+++ config/Database.js
@@ -1,1 +1,8 @@
 import { Sequelize } from "sequelize";
+
+const db = new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
+  host: "localhost",
+  dialect: "mysql"
+})
+
+export default db
--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 import express from "express"
+import db from "./config/Database.js"
 import * as dotenv from "dotenv"
 dotenv.config()
 
 const app = express()
+
+try {
+  await db.authenticate()
+  console.log("Connection has been established successfully")
+} catch (error) {
+  console.error("Unable to connect to the database : "+ error)
+}
 
 app.listen(process.env.PORT, () => {
   console.log("Server running at port " + process.env.PORT)
