87ee4069151c6f4b50f1c6c3233afb4b61d96a22
pujiragil <ragilsg164@gmail.com>
2022-12-18 21:39:38 +0700

    create register controller for users

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,3 +1,4 @@
+import bcrypt from "bcrypt"
 import Users from "../models/UserModel.js";
 
 export const getUsers = async (req, res) => {
@@ -8,4 +9,20 @@ export const getUsers = async (req, res) => {
     console.log(err)
     res.status(500).json({ err: err.message })
   }
+}
+
+export const registerUser = async (req, res) => {
+  const { name, email, password, confirmPassword } = req.body
+  if(!name || !email || !password || !confirmPassword) return res.status(400).json({ message: "Name, email, password and confirm password are required." })
+  if(password !== confirmPassword) return res.status(400).json({ message: "Password and confirm password doesn't match."})
+  const user = await Users.findOne({ where: { email }})
+  if(user) return res.status(400).json({ message: "User already exist." })
+  const hashedPassword = await bcrypt.hash(password, 10)
+  try {
+    await Users.create({ name, email, password: hashedPassword })
+    res.status(201).json({ message: "Register successfully"})
+  } catch (error) {
+    console.log(error)
+    res.status(500).json({ err: error.message })
+  }  
 }
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,9 @@
 import express from "express"
-import { getUsers } from "../controllers/UserController.js"
+import { getUsers, registerUser } from "../controllers/UserController.js"
 
 const router = express.Router()
 
 router.get("/", getUsers)
+router.post("/", registerUser)
 
 export default router
