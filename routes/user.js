7b008a5c61c61434a4e7dd39060670920d7fb028
pujiragil <ragilsg164@gmail.com>
2022-12-19 08:30:14 +0700

    add logout route

--- routes/user.js
+++ routes/user.js
@@ -1,6 +1,7 @@
 import express from "express"
 import * as userController from "../controllers/UserController.js"
 import * as refreshController from "../controllers/RefreshController.js"
+import * as logoutController from "../controllers/LogoutController.js"
 import verifyToken from "../middleware/verifyToken.js"
 
 const router = express.Router()
@@ -9,5 +10,6 @@ router.get("/", verifyToken, userController.handleGetUsers)
 router.post("/register",userController.handleRegister )
 router.post("/login", userController.handleLogin)
 router.get("/refresh", refreshController.handleRefreshToken)
+router.get("/logout", logoutController.handleLogout)
 
 export default router
