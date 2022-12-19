df82a22947e58e59d1bcbd543d73b9e8124e3afd
pujiragil <ragilsg164@gmail.com>
2022-12-19 08:01:37 +0700

    add new attributes for getUsers

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -4,7 +4,7 @@ import jwt from "jsonwebtoken";
 
 export const handleGetUsers = async (req, res) => {
   try {
-    const users = await Users.findAll();
+    const users = await Users.findAll({ attributes: ["id", "name", "email"]});
     res.status(200).json(users);
   } catch (err) {
     console.log(err);
